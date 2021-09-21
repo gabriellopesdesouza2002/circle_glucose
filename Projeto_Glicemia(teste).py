70609fdd9e1e2b9f49df4d787305e2eb55264dc0
Gabriel Lopes <gabriellopesdesouza2002@gmail.com>
2021-09-21 18:39:14 -0300

    será arrumado a parte para adicionar as glicemias em um periodo especifico

--- Projeto_Glicemia(teste).py
+++ Projeto_Glicemia(teste).py
@@ -1,11 +1,12 @@
-emails = ['gabriel', 'joao', 'reinaldo']
+from time import sleep
+
+emails = ['gabriel', 'joao', 'reinaldo', 'gab']
 senhas = ['123', 'gabriellopes', 'senhafraca']
 glicemias = []
 
-
 def inicio():
-    pergunta = input('Olá bem vindo ao sistema de controle de glicemia!\n'
-                     'Faça login => 1\n'
+    pergunta = input('\n\033[1;92mOlá bem vindo ao sistema de controle de glicemia!\033[m\n\n'
+                     'Faça login >>> 1\n'
                      'Faça um cadastro >>> 2\n'
                      'Sair do aplicativo >>> 3\n'
                      '>>> ')
@@ -17,7 +18,7 @@ def inicio():
         print('Tudo bem! Muito obrigado!')
         exit()
     else:
-        print('Erro! Tente novamente!')
+        print('\033[1;31mTente novamente!\033[m')
         inicio()
 
 
@@ -28,10 +29,10 @@ def fazerLogin():
     senha = input('Agora sua senha: ')
 
     if email in emails and senha in senhas:
-        print('Parabens! logado com sucesso!')
+        print('\n\033[1;92mParabéns! logado com sucesso!\033[m\n')
         glicemiaApp()
     else:
-        print('Tente novamente!')
+        print('\033[1;31mTente novamente!\033[m')
         fazerLogin()
 
 
@@ -62,27 +63,34 @@ def fazerCadastro():
         print('Tudo bem! Muito obrigado!')
         exit()
     else:
-        print('Tente novamente!')
+        print('\033[1;31mTente novamente!\033[m')
         fazerCadastro()
 
 
 def glicemiaApp():
-    print('Bem vindo ao App Controle de Glicemia!')
     pergunta = input('O que você quer fazer?\n'
                      'Adicionar um novo valor >>> 1\n'
                      'Ver suas medidas >>> 2\n'
                      'Ver um gráfico com as medidas >> 3\n'
-                     'Sair do aplicativo >>> 4\n')
+                     'Sair do aplicativo >>> 4\n'
+                     '>>> ')
 
     if pergunta == '1':
         adicionar_glicemia = input('Adicione a sua glicemia (Ex: 124): ')
+        periodo = input('Qual periodo foi feito a medição? '
+                        '')
         if adicionar_glicemia.isnumeric():
             glicemias.append(adicionar_glicemia)
-            print('Glicemia adicionada com sucesso!')
-            dnv = input('Você quer adicionar mais?\n'
-                        'Sim >>> 1'
-                        'Não >>> 2'
-                        'Sair do aplicativo totalmente >>> 3')
+            print('\033[1;32mGlicemia adicionada com sucesso!\033[m\n')
+            glicemiaApp()
+        else:
+            print(f'\033[1;31mO valor: {adicionar_glicemia} é inválido. Tente novamente!\033[m')
+            glicemiaApp()
+    elif pergunta == '2':
+        for glicemia in glicemias:
+            print(f'\033[1;32m{glicemia}\033[m\n')
+        sleep(2)
+        glicemiaApp()
     elif pergunta == '4':
         print('Tudo bem! Muito obrigado!')
         exit()
